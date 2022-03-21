39eed3e9a83f86c7228b2e9b9d5a15c2f301eecc
hoanghiep2001-coder <85659500+hoanghiep2001-coder@users.noreply.github.com>
2022-03-22 01:53:47 +0700

    Update mobile.js

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -1,10 +1,27 @@
 $(function () {
     var mobileBar = $('.bi.bi-list')
     var mobileMenu = $('.header__container-tablet-tab-clicked')
+    var html = $('html')
+    var mobileSlides = $('.image-slider')
+    var imgs = $('.mobile-slides-item')
+    var nextButtonSlide = $('.next-button')
+    var prevButtonSlide = $('.prev-button')
+    var idx = 0;
 
     var mobile = {
-        render: function() {
-
+        runSlide: function() {
+            // xử lý slideShow
+            function run() {
+                ++idx;
+                if (idx > imgs.length - 1) {
+                    idx = 0
+                }
+                $(mobileSlides).css({
+                    transform: `translateX(${-idx * 382}px)`
+                    
+                });
+            }
+            setInterval(run, 4000)
         },
         handleEvent: function() {
             $(mobileBar).click(function (e) { 
@@ -12,9 +29,57 @@ $(function () {
                 // $(mobileMenu).toggleClass('open');
                 $(mobileMenu).slideToggle();
             });
+
+            // $('.image-slider').slick();
+
+            // open button slide
+            $(mobileSlides).click(function (e) { 
+                e.stopPropagation();
+                $(nextButtonSlide).removeClass('close');
+                $(prevButtonSlide).removeClass('close');
+
+                $(nextButtonSlide).addClass('open');
+                $(prevButtonSlide).addClass('open');
+            });
+
+            $(html).click(function (e) {
+                $(nextButtonSlide).removeClass('open');
+                $(prevButtonSlide).removeClass('open');
+
+                $(nextButtonSlide).addClass('close');
+                $(prevButtonSlide).addClass('close');
+            });
+
+            // nút next Slide
+            $(nextButtonSlide).click(function (e) { 
+                e.stopPropagation()
+                ++idx;
+                if (idx > imgs.length - 1) {
+                    idx = 0
+                }
+                $(mobileSlides).css({
+                    transform: `translateX(${-idx * 382}px)`
+                    
+                });
+            });
+
+            // nút prev Slide
+            $(prevButtonSlide).click(function (e) { 
+                e.stopPropagation();
+                --idx;
+                if (idx < 0) {
+                    idx = imgs.length - 1
+                }
+                $(mobileSlides).css({
+                    transform: `translateX(${-idx * 382}px)`
+                    
+                });
+            });
+
         },
+
         start: function (param) {
-            this.render()
+            this.runSlide()
             this.handleEvent()
         }
     }
