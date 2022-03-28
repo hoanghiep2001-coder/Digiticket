1a16e0f3c4116d2f3d34ccb1422f90a1aa3bccf3
hoanghiep2001-coder <85659500+hoanghiep2001-coder@users.noreply.github.com>
2022-03-29 00:30:45 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,15 +1,75 @@
 $(function () {
     var dateRangeLeave = $('input[name="birthday"]')
     var dateRangeBack = $('.date-range-picker-back')
+    var tableCondensed = $('.table-condensed')
 
+    let html = $('html')
+    let bannerBannerOptions = $('.banner-options-dropdown')
 
+    let calendarTable = $('.startDate')
+    let endCalendarTable = $('.endDate')
+    
     var dateRangePicker = {
         config: function() {
-            $('input[name="birthday"]').daterangepicker({
-                singleDatePicker: true,
-                showDropdowns: true,
-                minYear: 1901,
-                maxYear: parseInt(moment().format('YYYY'),10)
+              $('input[name="birthday"]').daterangepicker({
+                "locale": {
+                    "format": "DD/MM/YYYY",
+                    "separator": " - ",
+                    "applyLabel": "Apply",
+                    "cancelLabel": "Cancel",
+                    "fromLabel": "From",
+                    "toLabel": "To",
+                    "customRangeLabel": "Custom",
+                    "weekLabel": "W",
+                    "daysOfWeek": [
+                        "CN",
+                        "T2",
+                        "T3",
+                        "T4",
+                        "T5",
+                        "T6",
+                        "T7"
+                    ],
+                    "monthNames": [
+                        "Tháng 1",
+                        "Tháng 2",
+                        "Tháng 3",
+                        "Tháng 4",
+                        "Tháng 5",
+                        "Tháng 6",
+                        "Tháng 7",
+                        "Tháng 8",
+                        "Tháng 9",
+                        "Tháng 10",
+                        "Tháng 11",
+                        "Tháng 12",
+                    ],
+                    "firstDay": 1
+                },
+                "singleDatePicker": true,
+                "autoApply": true,
+                "maxSpan": {
+                    "days": 7
+                },
+                "startDate": "28/03/2022",
+                "endDate": "28/03/2024",
+                "minDate": "28/03/2022",
+                "maxDate": "28/03/2023"
+            },
+             function(start, end, label) {
+              
+            });
+
+              $.each(bannerBannerOptions, function (indexInArray, dropdown) { 
+                $(html).click(function (e) { 
+                    e.preventDefault();
+                    $(dropdown).removeClass('open');
+                });
+              });
+
+              // done date đầu tiên sẽ xổ ra date thứ 2
+              $(calendarTable).change(function (e) { 
+                $(endCalendarTable).trigger("click");
               });
         },
         start: function (param) {
@@ -19,3 +79,19 @@ $(function () {
 
     dateRangePicker.start()
 });
+
+
+// $(document).on('click', 'li.item', function(e){
+//     e.preventDefault();
+//     let $this = $(this);
+//     let $parent = $this.closet('.input-container');
+//     let $input = $parent.find('input.class');
+//     let $dropdown = $parent.find('dropdown.menu');
+
+//     let _txt = $this.find('h3').text();
+//     let _airport = $this.find('p').text();
+//     $input.val(`${_txt} - ${_airport}`).trigger('change');
+//     $dropdown.slideUp(500);
+//     $dropdown.toggleClass()
+    
+// });
